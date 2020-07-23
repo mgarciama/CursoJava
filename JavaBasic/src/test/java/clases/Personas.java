151d97c80e94e55703663ffb2a00531caed6c7a5
mgarciama <52792767+mgarciama@users.noreply.github.com>
2020-07-23 11:02:53 +0200

    Add files via upload

---
+++ JavaBasic/src/test/java/clases/Personas.java
@@ -0,0 +1,15 @@
+package clases;
+
+public class Personas {
+	
+	
+	//atributos de la clases
+	String nombre;
+	String apellido;
+	
+	//metodos
+	public void desplegarInformación() {
+		System.out.println("Nombre: " + nombre);
+		System.out.println("Apellidos: " + apellido);
+	}
+}
---
+++ JavaBasic/src/test/java/clases/PruebaPersona.java
@@ -0,0 +1,19 @@
+package clases;
+
+public class PruebaPersona {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//variable 
+		Personas persona1;
+		//creación de objetos
+		persona1  = new Personas();
+		//efecto en la memoria
+		persona1.nombre = "Manuel";
+		persona1.apellido = "García";
+		persona1.desplegarInformación();
+		System.out.println("hola");
+	}
+
+}
